7c52853291315447f4cd905f7f5fcfd64e615728
Jorge Reus <j-g1996@live.com>
2018-07-14 16:21:35 -0500

    Finished Hellows

--- hellowebservice/src/main/kotlin/com/reus/ws/soap/HelloWsKT.kt
+++ hellowebservice/src/main/kotlin/com/reus/ws/soap/HelloWsKT.kt
@@ -1,9 +1,11 @@
 package com.reus.ws.soap
 
+import org.apache.cxf.feature.Features
 import javax.jws.WebMethod
 import javax.jws.WebService
 
 @WebService
+@Features(features = ["org.apache.cxf.feature.LoggingFeature"])
 open class HelloWsKT {
 
     @WebMethod
