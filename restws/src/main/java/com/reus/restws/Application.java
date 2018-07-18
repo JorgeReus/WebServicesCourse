186a708c47f19ce6c2b491abc85e6296f81a6161
reus <j-g1996@live.com>
2018-07-18 06:09:20 -0500

    Added war support

--- restws/src/main/java/com/reus/restws/Application.java
+++ restws/src/main/java/com/reus/restws/Application.java
@@ -2,9 +2,20 @@ package com.reus.restws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    /*
+    To make use of spring's servlet 3.0 support and configure the application when
+    is launched by the servlet container
+    * */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
